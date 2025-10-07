669ed00720093ab99966110b12d6b0be2a3dc8b1
Rafi570 <hasan.cse570@gmail.com>
2025-10-07 21:46:06 +0600

    create basic structure

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,31 @@
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import ErrorPage from "../page/ErrorPage/ErrorPage";
+import Root from "../page/Root/Root";
+import Apps from "../page/Apps/Apps";
+import Home from '../page/Home/Home'
+import Installetions from "../page/Install/Installetions";
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: Root,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+    {
+      index:true,
+      path:'/',
+      Component: Home
+
+    },
+    {
+      path: '/apps',
+      Component:Apps
+    },
+    {
+      path:'/installation',
+      Component:Installetions
+
+    }
+  ],
+  },
+]);
---
+++ src/page/Root/Root.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import { Outlet } from "react-router";
+import Footer from "../../components/Footer/Footer";
+
+const Root = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="bg-gray-50">
+        <div className="">
+          <Outlet></Outlet>
+        </div>
+      </div>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+export default Root;
